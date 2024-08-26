98d0f5d8b7a6030a33aa041c2e3fbc1c869a61b1
lukettoOoO <mihut.luca@yahoo.com>
2024-08-26 20:41:14 +0300

    commit 2

--- 06/Assembler.C
+++ 06/Assembler.C
@@ -4,13 +4,29 @@
 #include <ctype.h>
 
 #define MAX_SIZE_ASM 655360
+#define MAX_SIZE_ASM_LINE 1024
 #define MAX_SIZE_HACK 65536
 
-char asmb[MAX_SIZE_ASM][100];
+char asmb[MAX_SIZE_ASM][MAX_SIZE_ASM_LINE];
 char hack[MAX_SIZE_HACK][MAX_SIZE_HACK];
-int asmb_size=0;
 
-void open_asm(const char* filename)
+typedef enum{
+    A_COMMAND,
+    C_COMMAND,
+    L_COMMAND
+}command_type;
+
+bool is_empty_line(const char *line) 
+{
+    for (int i=0; i<(int)strlen(line); i++)
+    {
+        if (line[i]!=' ' && line[i]!='\t' && line[i]!='\n' && line[i]!='\r')
+            return false;
+    }
+    return true;
+}
+
+int open_asm(const char* filename)
 {
     FILE *f=NULL;
     if((f=fopen(filename, "r"))==NULL)
@@ -18,40 +34,118 @@ void open_asm(const char* filename)
         fprintf(stderr, "error opening .asm file\n");
         exit(EXIT_FAILURE);
     }
-    printf("file opened successfully\n");
-    char line[MAX_SIZE_ASM];
-    int asmb_size=0;
-    while(fgets(line, MAX_SIZE_ASM, f)!=NULL)
+    
+    char line[MAX_SIZE_ASM_LINE];
+    int size=0;
+    while(fgets(line, MAX_SIZE_ASM_LINE, f)!=NULL)
     {
-        if(strlen(line)>100)
-            {
-                fprintf(stderr, "lines of .asm file cannot exceed 100 character limit\n");
-                exit(EXIT_FAILURE);
-            }
-        strcpy(asmb[asmb_size], line);
-        asmb_size++;
+        //printf("%d %s", size, line);
+        if((line[0]=='/' && line[1]=='/') || is_empty_line(line))
+            continue;
+        strcpy(asmb[size], line);
+        size++;
     }
-    printf("asmb written successfully\n");
+    printf(".asm file read successfully\n");
+
     if(fclose(f)!=0)
     {
         fprintf(stderr, "error closing .asm file\n");
         exit(EXIT_FAILURE);
     }
-    
+    return size;
 }
 
-void write_hack(const char *filename)
+void symbol(char *s, char *line, command_type cmd)
+{
+    s[0]='\0';
+    if(cmd==A_COMMAND)
+    {
+        char *p=strtok(line, "@");
+        strcpy(s, p);
+    }
+    else
+    {
+        int k=0;
+        for(int i=0; i<(int)strlen(line); i++)
+        {
+            if(line[i]!='(' && line[i]!=')')
+            {
+                s[k]=line[i];
+                k++;
+            }
+        }
+    }
+}
+
+void dest(char *m, char *line)
+{
+    m[0]='\0';
+    if(strchr(line, '=')==NULL)
+        strcpy(m, "null");
+    if(line[0]=='M')
+        strcpy(m, "M");
+    if(line[0]=='D')
+        strcpy(m, "D");
+    if(line[0]=='M' && line[1]=='D')
+        strcpy(m, "MD");
+    if(line[0]=='A')
+        strcpy(m, "A");
+    if(line[0]=='A' && line[1]=='M')
+        strcpy(m, "AM");
+    if(line[0]=='A' && line[1]=='D')
+        strcpy(m, "AD");
+    if(line[0]=='A' && line[1]=='M' && line[2]=='D')
+        strcpy(m, "AMD");
+}
+
+void comp(char *m, char *line)
+{
+    m[0]='\0';
+    int ok_eq=0, ok_c=0;
+    for(int i=0; i<(int)strlen(line); i++)
+    {
+        if(line[i]=='=')
+            ok_eq=1;
+        if(line[i]==';')
+            ok_c=1;
+    }
+    char c[1024][1024];
+    char line_copy[strlen(line) + 1];
+    strcpy(line_copy, line);
+    char *p=strtok(line_copy, "=;");
+    int n=0;
+    while(p!=NULL)
+    {
+        strcpy(c[n], p);
+        n++;
+        p=strtok(NULL, "=;");
+    }
+    if(ok_eq==1 && ok_c==0)
+        strcpy(m, c[1]);
+    if(ok_eq==0 && ok_c==1)
+        strcpy(m, c[0]);
+    if(ok_eq==1 && ok_c==1)
+        strcpy(m, c[1]);
+}
+
+command_type commandType(char *line)
+{
+    //A_COMMAND
+    if(line[0]=='@')
+        return A_COMMAND;
+    //L_COMMAND
+    if(line[0]=='(')
+        return L_COMMAND;
+    //C_COMMAND
+    return C_COMMAND;
+}
+
+void write_hack(char *filename, int size)
 {
     FILE *f=NULL;
     char filename_hack[100];
-    int i;
-    for(i=0; i<strlen(filename); i++)
-    {
-        if(filename[i]=='.')
-            break;
-        else
-            filename_hack[i]=filename[i];
-    }
+    char *p=strtok(filename, ".");
+    strcpy(filename_hack, p);
     strcat(filename_hack, ".hack");
     
     if((f=fopen(filename_hack, "w"))==NULL)
@@ -59,9 +153,38 @@ void write_hack(const char *filename)
         fprintf(stderr, "error opening .hack file\n");
         exit(EXIT_FAILURE);
     }
-    
-    //testing the file conversion
-    fprintf(f, "%s", asmb);
+
+    //copy paste of the .asm file:
+    /*for(int i=0; i<size; i++)
+    {
+        fprintf(f, "%s", asmb[i]);
+    }*/
+   
+   //prompt:
+   char sb[1024];
+   char mnd[8];
+   char mnc[32];
+    for(int i=0; i<size; i++)
+    {
+        if(commandType(asmb[i])==A_COMMAND)
+        {
+            symbol(sb, asmb[i], A_COMMAND);
+            fprintf(f, "commandType: A_COMMAND, symbol: %s", sb);
+        }
+        else if(commandType(asmb[i])==C_COMMAND)
+        {
+            dest(mnd, asmb[i]);
+            comp(mnc, asmb[i]);
+            fprintf(f, "commandType: C_COMMAND, dest: %s, comp: %s", mnd, mnc);
+        }
+        else if(commandType(asmb[i])==L_COMMAND)
+        {
+            symbol(sb, asmb[i], L_COMMAND);
+            fprintf(f, "commandType: L_COMMAND, symbol: %s", sb);
+        }
+    }
+
+    printf(".hack file written successfully\n");
 
     if(fclose(f)!=0)
     {
@@ -78,7 +201,7 @@ int correct_input_format(char *filename)
         return 0;
     }
     int ok=0;
-    for(int i=0; i<strlen(filename); i++)
+    for(int i=0; i<(int)strlen(filename); i++)
     {
         if(filename[i]=='.')
             ok=1;
@@ -93,11 +216,13 @@ int correct_input_format(char *filename)
 
 int main(int argc, char **argv)
 {
+    printf("~~~Luca's Assembler for Hack~~~\n!!! Line size must be max 1024 characters\n!!! Do not include indentation\n");
+    int asmb_size;
     if(correct_input_format(argv[1])==0)
     {
         exit(EXIT_FAILURE);
     }
-    open_asm(argv[1]);
-    write_hack(argv[1]);
+    asmb_size=open_asm(argv[1]);
+    write_hack(argv[1], asmb_size);
     return 0;
 }
---
+++ 06/Prog.hack
@@ -0,0 +1,9 @@
+commandType: A_COMMAND, symbol: 2
+commandType: C_COMMAND, dest: D, comp: A
+commandType: A_COMMAND, symbol: 3
+commandType: C_COMMAND, dest: D, comp: D+A
+commandType: A_COMMAND, symbol: 0
+commandType: C_COMMAND, dest: M, comp: D
+commandType: L_COMMAND, symbol: LOOP
+commandType: A_COMMAND, symbol: LOOP
+commandType: C_COMMAND, dest: null, comp: 0
